9cb768e636dcfbc57b548a77e0243a9a11612f19
Isacco Pironato <158030915+IW3Rpyro@users.noreply.github.com>
2024-12-20 16:08:11 +0000

    input output test in C

--- Hello/hello.c
+++ Hello/hello.c
@@ -9,10 +9,20 @@ main()
     // \n is shortcut for new line 
 
     printf("\n");
-    printf("Hello World!!!\n");
+    printf("Hello World!!!\n\n\n");
 
-    return 0;
+    // variable of 32 characters
+    char name[32];
 
+    // ask for name 
+    printf("Enter a name: ");
+
+    // read the input of the user 
+    scanf("%s", name);
+
+
+    printf("\n hello \t %s \n\n\n", name);
     /* return 0: indicated that the program is terminated succesfully
     */
+    return 0;
 }
