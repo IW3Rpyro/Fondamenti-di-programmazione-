1433ac5f4f7e3907734b4b86b1a458aa6e6f7c2d
Isacco Pironato <158030915+IW3Rpyro@users.noreply.github.com>
2024-12-20 15:51:33 +0000

    hello world in c

---
+++ Hello/hello.c
@@ -0,0 +1,18 @@
+
+#include <stdio.h>
+#include <math.h>
+// main function - the entry point
+// the retrn value of the main is an integer 
+main()
+{
+    // print the message "Hello World!!!" to the console 
+    // \n is shortcut for new line 
+
+    printf("\n");
+    printf("Hello World!!!\n");
+
+    return 0;
+
+    /* return 0: indicated that the program is terminated succesfully
+    */
+}
